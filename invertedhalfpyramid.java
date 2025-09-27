fe4d503325ea6e077c5b1254c48715b01962314b
Ashish Kolekar <143067353+ashishk024@users.noreply.github.com>
2025-09-27 21:47:47 +0530

    Add files via upload

---
+++ invertedhalfpyramid.java
@@ -0,0 +1,24 @@
+// inverted half pyramid
+// 12345
+// 1234
+// 123
+// 12
+// 1
+
+public class invertedhalfpyramid {
+    public static void inverted_half_pyramid(int n) {
+        for (int i = 1; i <= n; i++) {
+            for(int j = 1; j <= n-i+1; j++) {
+                System.out.print(j);
+            }
+            for (int j = 1; j <= i; j++) {
+                System.out.print(" ");
+            }
+            System.out.println();
+        }
+    }
+
+    public static void main(String[] args) {
+        inverted_half_pyramid(6);
+    }
+}
